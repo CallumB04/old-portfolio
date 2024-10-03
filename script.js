44a9987186e4bfddb21b7b647dde23aecf5c7e1c
Cal <callumburgoyne04@gmail.com>
2024-10-03 15:36:12 +0100

    Improving navbar highlighting

--- script.js
+++ script.js
@@ -3,33 +3,59 @@ const navbarAbout = document.getElementById("nav-about");
 const navbarProjects = document.getElementById("nav-projects");
 
 
-// Adding selected class on navbar when option chosen
+/* Highlighting currently open section of webpage on the navbar */
+
+// -- HOME --
 navbarHome.addEventListener("click", () => {
     navbarAbout.classList.remove("selected");
     navbarProjects.classList.remove("selected");
+    navbarHome.classList.remove("highlight");
     navbarHome.classList.add("selected");
 });
+// -- ABOUT --
 navbarAbout.addEventListener("click", () => {
     navbarHome.classList.remove("selected");
     navbarProjects.classList.remove("selected");
+    navbarAbout.classList.remove("highlight");
     navbarAbout.classList.add("selected");
 });
+// -- PROJECTS --
 navbarProjects.addEventListener("click", () => {
     navbarAbout.classList.remove("selected");
     navbarHome.classList.remove("selected");
+    navbarProjects.classList.remove("highlight");
     navbarProjects.classList.add("selected");
 });
 
-// Temporary highlight on navbar when option hovered
-navbarHome.addEventListener("mouseover",
-    () => navbarHome.classList.add("highlight"));
+
+/* Temporary highlights navbar options when hovered */
+
+// -- HOME --
+navbarHome.addEventListener("mouseover", () => {
+    if (!navbarHome.classList.contains("selected")){
+        navbarHome.classList.add("highlight")
+    }
+});
 navbarHome.addEventListener("mouseout",
-    () => navbarHome.classList.remove("highlight"));
-navbarAbout.addEventListener("mouseover",
-    () => navbarAbout.classList.add("highlight"));
+    () => navbarHome.classList.remove("highlight")
+);
+
+// -- ABOUT --
+navbarAbout.addEventListener("mouseover", () => {
+    if (!navbarAbout.classList.contains("selected")){
+        navbarAbout.classList.add("highlight")
+    }
+});
 navbarAbout.addEventListener("mouseout",
-    () => navbarAbout.classList.remove("highlight"));
-navbarProjects.addEventListener("mouseover",
-    () => navbarProjects.classList.add("highlight"));
+    () => navbarAbout.classList.remove("highlight")
+);
+
+// -- PROJECTS --
+navbarProjects.addEventListener("mouseover", () => {
+    if (!navbarProjects.classList.contains("selected")){
+        navbarProjects.classList.add("highlight")
+    }
+});
 navbarProjects.addEventListener("mouseout",
-    () => navbarProjects.classList.remove("highlight"));
+    () => navbarProjects.classList.remove("highlight")
+);
